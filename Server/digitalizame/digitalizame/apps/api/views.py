5cd4d32d81c974dc2b4d82fd514912990cedf12c
eveR Vasquez <pever@unsm.edu.pe>
2016-01-08 05:38:17 -0500

    update

--- Server/digitalizame/digitalizame/apps/api/views.py
+++ Server/digitalizame/digitalizame/apps/api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from django.db import IntegrityError, transaction
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import viewsets, status
 from .mixins import DefaultViewSetMixin
 from .models import Site, Category
 from .serializers import SiteSerializer, CategorySerialier
@@ -16,3 +19,24 @@ class SiteViewSet(DefaultViewSetMixin, viewsets.ModelViewSet):
     serializer_class = SiteSerializer
     search_fields = ('description', 'tags', 'category', 'creator_by')
     ordering_fields = '__all__'
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        try:
+            with transaction.atomic():
+                print(request.data)
+                return Response({}, status=status.HTTP_200_OK)
+
+        except IntegrityError as e:
+            error = {
+                "result": "Error",
+                "message": str(e)
+            }
+
+        except ValidationError as e:
+            error = {
+                "result": "Error",
+                "message": str(e)
+            }
+
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)
