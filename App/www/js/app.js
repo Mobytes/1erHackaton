fe76ec38a12eeb49504c79067cdb6929da7ee663
brapastor <brapastor@gmail.com>
2016-01-08 05:34:12 -0500

    add login

--- App/www/js/app.js
+++ App/www/js/app.js
@@ -91,7 +91,17 @@
             controller: 'ConfigCtrl'
           }
         }
-      });
+      })
+
+  .state('app.login', {
+      url: '/login',
+      views: {
+        'tab-account': {
+          templateUrl: 'templates/login.html'
+          //controller: 'LoginCtrl'
+        }
+      }
+    });
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/map');
