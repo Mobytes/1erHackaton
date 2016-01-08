4ae5868b38e5db78b99327678447e0b945271e96
eveR Vasquez <pever@unsm.edu.pe>
2016-01-08 06:13:03 -0500

    update user

--- Server/digitalizame/digitalizame/apps/api/serializers.py
+++ Server/digitalizame/digitalizame/apps/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Rating, Site, Picture, Like, Category
 
@@ -30,3 +31,18 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ('user', 'site')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('password', 'first_name', 'last_name', 'email',)
+        write_only_fields = ('password',)
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
+
+    def restore_object(self, attrs, instance=None):
+        # call set_password on user object. Without this
+        # the password will be stored in plain text.
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
--- Server/digitalizame/digitalizame/apps/api/urls.py
+++ Server/digitalizame/digitalizame/apps/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import SiteViewSet, CategoryViewSet
+from .views import SiteViewSet, CategoryViewSet, UserView
 
 
 router = routers.DefaultRouter()
 router.register(r'sites', SiteViewSet)
 router.register(r'categories', CategoryViewSet)
+router.register(r'accounts', UserView, 'list')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
